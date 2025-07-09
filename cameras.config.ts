42e45ed260cff2c645f50470a52d76d40be77792
Guilherme Froes <94195233+smileee@users.noreply.github.com>
2025-07-09 19:48:24 -0400

    Just one camera

--- cameras.config.ts
+++ cameras.config.ts
@@ -9,10 +9,10 @@ export const CAMERAS: Camera[] = [
     id: '1',
     name: 'Cam 1 (BOX CAM)',
     rtspUrl: 'rtsp://192.168.9.232:554',
-  },
-  {
-    id: '2',
-    name: 'Cam 2 (PI CAM)',
-    rtspUrl: 'rtsp://192.168.9.161:554',
-  },
+  }
+  // ,{
+  //   id: '2',
+  //   name: 'Cam 2 (PI CAM)',
+  //   rtspUrl: 'rtsp://192.168.9.161:554',
+  // }
 ]; 
